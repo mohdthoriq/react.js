753207cfaa34a9e8e733f1999c1195755cabb929
Thoriq.Cyber <mohdthoriq24@gmail.com>
2025-10-23 10:13:13 +0700

    feat: Form

--- minggu4/day2/tugas/src/components/Form.tsx
+++ minggu4/day2/tugas/src/components/Form.tsx
@@ -7,3 +7,29 @@ interface FormData {
 interface FormProps {
     onSubmit: ( data: FormData ) => void;
 }
+
+export const Form = ({ onSubmit }: FormProps) => {
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const formData = new FormData(e.currentTarget)
+        const username = formData.get('username') as string;
+        const age = Number(formData.get('age'));
+        const newsletter = formData.get('newsletter') === 'on';
+
+        if (!username || isNaN(age)) return;
+
+        onsubmit({ username, age, newsletter });
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input name='username' placeholder="username" required />
+            <input name='age' placeholder="Age" required />
+
+            <label>
+                <input name='newsletter' type='checkbox' />
+            </label>
+            <button type="submit">Submit</button>
+        </form>
+    )
+}
